e7131cc761055c80dac07404314fa69d6a548a09
Evan Walker <walke208@mail.chapman.edu>
2017-09-27 03:22:25 +0000

    created module

---
+++ cplane.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import abscplane
+
+###
+# Name: Evan A Walker, Tim lastName
+# Student ID: 01932978 , 
+# Email: walke208@mail.chapman.edu , TimEmailHere
+# Course: CS510 Fall 2017
+# Assignment: Classwork 5
+###
+
+class ListComplexPlane(AbsComplexPlane):
+    def __init__(self, plane):
+        self.plane = 
