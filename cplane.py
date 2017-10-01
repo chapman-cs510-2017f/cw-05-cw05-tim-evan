a9119ce3643e8933266ddc92ac76ca4dcff0afe9
TimFrenzel <31460674+TimFrenzel@users.noreply.github.com>
2017-10-01 14:40:06 -0700

    did some changes, ToDo: "store a list of functions"

--- cplane.py
+++ cplane.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from abscplane import AbsComplexPlane
-
 ###
-# Name: Evan A Walker, Tim lastName
+# Name: Evan A Walker, Tim Frenzel
 # Student ID: 01932978 , 
-# Email: walke208@mail.chapman.edu , TimEmailHere
+# Email: walke208@mail.chapman.edu , frenz102@mail.chapman.edu
 # Course: CS510 Fall 2017
 # Assignment: Classwork 5
 ###
 
+from abscplane import AbsComplexPlane
 
 class ListComplexPlane(AbsComplexPlane):
     def __init__(self,xmin,xmax,xlen,ymin,ymax,ylen):
@@ -20,28 +19,63 @@ class ListComplexPlane(AbsComplexPlane):
         self.ymin  = ymin
         self.ymax  = ymax
         self.ylen  = ylen
-        #fs    =
+
         dx = (xmax - xmin)/(xlen - 1)
         dy = (ymax - ymin)/(ylen - 1)
-        print(dx)
-        listoflists = []
+
+        self.plane = []
+        self.fs = []
+
+    def __generate_planegrid(self,xmin,xmax,xlen,ymin,ymax,ylen,dx,dy):
+        """Builds complex plane"""
         for i in range(xmin,xmax):
             sublist = []
             for j in range(ymin,ymax):
                 sublist.append((xmin + i*dx)+(ymin +j*dy)*1j)
-            listoflists.append(sublist)
-        print(listoflists)
-        self.plane = listoflists
+            self.plane.append(sublist)
+        print(self.plane)
+        return self.plane
+#ToDo: integrate functions...
 
     def refresh(self):
-        pass
+        """restores to its original grid values"""
+        self.plane = []
+        self.fs = []
+        x = self.xmin
+        y = self.ymin
+        xl= self.xlen
+        yl = self.ylen
+        dx = (xmax - x)/(xl-1)
+        dy = (ymax - y)/(y1-1)
 
     def apply(self, f):
-        pass
+        self.f = f
+        self.fs.append(self.f)
+
+        for i in range(self.xlen):
+            for j in range(self.ylen):
+                self.plane[i][j] = f(self.plane[i][j]) ### Replace with the grid function
+        self.fs.append(f)
+
 
     def zoom(self,xmin,xmax,xlen,ymin,ymax,ylen):
-        pass
+        self.xmin  = xmin
+        self.xmax  = xmax
+        self.xlen  = xlen
+        self.ymin  = ymin
+        self.ymax  = ymax
+        self.ylen  = ylen
+
+        dx = (xmax - x)/(xlen-1)
+        dy = (ymax - y)/(y1en-1)
+
+        """Builds complex plane"""
+        for i in range(xmin,xmax):
+            sublist = []
+            for j in range(ymin,ymax):
+                sublist.append((xmin + i*dx)+(ymin +j*dy)*1j)
+            self.plane.append(sublist
+#ToDo: integrate functions...
 
 
-
-myPlane = ListComplexPlane(0,10,11,0,10,11)
+#myPlane = ListComplexPlane(0,10,11,0,10,11)
