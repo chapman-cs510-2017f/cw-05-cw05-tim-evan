01afd02c004b3ae7217d02870c4282dbcac00706
Evan Walker <walke208@mail.chapman.edu>
2017-10-02 21:30:22 +0000

    implementing fxns, rough edit

--- cplane.py
+++ cplane.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import numpy as np
+from matplotlib import pyplot as plt
 from abscplane import AbsComplexPlane
 
 ###
@@ -20,28 +22,59 @@ class ListComplexPlane(AbsComplexPlane):
         self.ymin  = ymin
         self.ymax  = ymax
         self.ylen  = ylen
-        #fs    =
+        self.fs    = []
         dx = (xmax - xmin)/(xlen - 1)
         dy = (ymax - ymin)/(ylen - 1)
-        print(dx)
         listoflists = []
         for i in range(xmin,xmax):
             sublist = []
             for j in range(ymin,ymax):
                 sublist.append((xmin + i*dx)+(ymin +j*dy)*1j)
             listoflists.append(sublist)
-        print(listoflists)
         self.plane = listoflists
 
+    def setPlane(self,xmin,xmax,xlen,ymin,ymax,ylen):
+        dx = (xmax - xmin)/(xlen - 1)
+        dy = (ymax - ymin)/(ylen - 1)
+        listoflists = []
+        for i in range(xmin,xmax):
+            sublist = []
+            for j in range(ymin,ymax):
+                sublist.append((xmin + i*dx)+(ymin +j*dy)*1j)
+            listoflists.append(sublist)
+        self.plane = listoflists
+        pass
+
     def refresh(self):
+        dx = (self.xmax - self.xmin)/(self.xlen - 1)
+        dy = (self.ymax - self.ymin)/(self.ylen - 1)
+        listoflists = []
+        for i in range(self.xmin,self.xmax):
+            sublist = []
+            for j in range(self.ymin,self.ymax):
+                sublist.append((self.xmin + i*dx)+(self.ymin +j*dy)*1j)
+            listoflists.append(sublist)
+        self.plane = listoflists
         pass
 
     def apply(self, f):
         pass
 
     def zoom(self,xmin,xmax,xlen,ymin,ymax,ylen):
+        self.xmin  = xmin
+        self.xmax  = xmax
+        self.xlen  = xlen
+        self.ymin  = ymin
+        self.ymax  = ymax
+        self.ylen  = ylen
+        #self.fs    = fs
+        self.plane = setPlane(self,xmin,xmax,xlen,ymin,ymax,ylen)
+        pass
+    def printTable(self):
+        for s in self.plane:
+            print(*s)
         pass
 
 
-
-myPlane = ListComplexPlane(0,10,11,0,10,11)
+myPlane = ListComplexPlane(-10,10,21,-10,10,21)
+myPlane.printTable()
